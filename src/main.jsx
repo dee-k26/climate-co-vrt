8c3be3287fdfe1955b81b89fa31f0236ee7dfb6d
Daniel Knight <dan@knight-co.au>
2025-05-01 18:03:56 +1000

    fix GTM code in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import App from './App'
 import TagManager from 'react-gtm-module';
 import './index.css'
 
-const tagManagerArgs = {
+TagManager.initialize({
   gtmId: 'GTM-TM4MCMC6'
-};
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
